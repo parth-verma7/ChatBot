9428a5fe11475dbd6ea99030e6bd5982d3b1ec78
Parth Verma <96720577+parth-verma7@users.noreply.github.com>
2023-02-09 15:34:06 +0530

    Add files via upload

---
+++ table.sql
@@ -0,0 +1,11 @@
+create table flight_data(
+    PassengerID int,
+    PassengerName varchar(20) not null,
+    PassengerPhoneNo varchar(20) not null,
+    PassengerEmail varchar(255) not null,
+    PassengerSex varchar(20) not null,
+    PassengerSourceCity varchar(20) not null,
+    PassengerDestinationCity varchar(20) not null
+    BookingDate DATE not null,
+);
+
